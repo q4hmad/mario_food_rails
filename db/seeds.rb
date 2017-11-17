daf3687d74d97dc9049c85a6430d6162020c66ea
Qudsia Ahmad <q4hmad@gmail.com>
2017-11-17 11:25:52 -0800

    seeded databases

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Commerce.product_name,
+                  cost: Faker::Commerce.price,
+                  country_of_origin: Faker::Address.country)
+end
+
+250.times do |index|
+  Review.create!(author: Faker::Name.name_with_middle,
+                 content_body: Faker::Seinfeld.quote,
+                 rating: Faker::Number.between(1,5),
+                 product_id: Faker::Number.between(Product.first.id, Product.last.id))
+end
+
+                 p "Created #{Product.count} products and reviews"
