b453254854d196766a08c090157efc55e8371590
Qudsia Ahmad <q4hmad@gmail.com>
2017-11-17 15:53:26 -0800

    seven passing specs

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   validates :author, :presence => true
+  validates :rating, :presence => true
+  validates :content_body, :presence => true
+
+
 end
