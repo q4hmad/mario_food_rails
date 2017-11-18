955f97b65d7dc528cf66343a58e5b833c9d94b7d
Qudsia Ahmad <q4hmad@gmail.com>
2017-11-17 16:59:25 -0800

    character length validation passing

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,8 @@ class Review < ActiveRecord::Base
   validates :author, :presence => true
   validates :rating, :presence => true
   validates :content_body, :presence => true
+  validates_length_of :content_body, :in => 50..250, :allow_nil => true
 
+  
 
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,5 +4,9 @@ describe Review do
   it { should belong_to :product }
   it { should validate_presence_of :author }
   it { should validate_presence_of :content_body }
-  it { should validate_presence_of :rating}
-end
+  it { should validate_presence_of :rating }
+
+  it { should validate_length_of(:content_body).
+      is_at_least(50).
+      on(:create) }
+    end
