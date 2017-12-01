0e60930d446b0ee48f1eb32641ac9334ae755856
Qudsia Ahmad <q4hmad@gmail.com>
2017-12-01 11:27:45 -0800

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'products#index'
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
+
   resources :products do
    resources :reviews, :except => [:show, :index]
   end
